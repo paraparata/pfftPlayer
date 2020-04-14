af605c907fd224cf18a6c6d4cb9581588e18db52
paraparata <kido.151@gmail.com>
2020-04-14 18:04:27 +0700

    deploy

---
+++ precache-manifest.0515985cbbde35fde7fc5d3f1063fd0a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22c0bb8f5ccd26ba9ee510b7579018e",
+    "url": "/pfftPlayer/bgMusic.wav"
+  },
+  {
+    "revision": "d5ed79d0dd7413e9a06f",
+    "url": "/pfftPlayer/css/about.b980232c.css"
+  },
+  {
+    "revision": "b1c4cbcb4e92bb505442",
+    "url": "/pfftPlayer/css/app.c4ceac5b.css"
+  },
+  {
+    "revision": "aba6174c0518a3797a5c",
+    "url": "/pfftPlayer/css/chunk-435e4632.672622d8.css"
+  },
+  {
+    "revision": "2425fd3a87d5da8b5e40",
+    "url": "/pfftPlayer/css/chunk-5c5060e6.b0756bcd.css"
+  },
+  {
+    "revision": "ffded846b19195541893",
+    "url": "/pfftPlayer/css/chunk-vendors.6bb67471.css"
+  },
+  {
+    "revision": "07104c7eec0696733b91b3a628bb428b",
+    "url": "/pfftPlayer/index.html"
+  },
+  {
+    "revision": "d5ed79d0dd7413e9a06f",
+    "url": "/pfftPlayer/js/about.4d7e0b9d.js"
+  },
+  {
+    "revision": "b1c4cbcb4e92bb505442",
+    "url": "/pfftPlayer/js/app.14f1d6cb.js"
+  },
+  {
+    "revision": "aba6174c0518a3797a5c",
+    "url": "/pfftPlayer/js/chunk-435e4632.7ac00249.js"
+  },
+  {
+    "revision": "2425fd3a87d5da8b5e40",
+    "url": "/pfftPlayer/js/chunk-5c5060e6.793cba62.js"
+  },
+  {
+    "revision": "ffded846b19195541893",
+    "url": "/pfftPlayer/js/chunk-vendors.6a0fbdfb.js"
+  },
+  {
+    "revision": "d46d9d704e1b1b4f0c12109eeb0f9875",
+    "url": "/pfftPlayer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pfftPlayer/robots.txt"
+  }
+]);
